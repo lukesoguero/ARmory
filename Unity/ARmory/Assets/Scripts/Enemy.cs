96f3429123fb11b044cdea84713d5f9f80c47c18
Luke Soguero <lukesoguero@gmail.com>
2021-09-18 19:55:17 -0500

    enemy asset and script

---
+++ Unity/ARmory/Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float moveSpeed = 5.0f;
+    public float minDist = 2.0f;
+
+    private Animator animator;
+
+    void Start() {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // follow player
+        transform.LookAt(new Vector3(player.position.x, transform.position.y, player.position.z));
+        if (Vector3.Distance(transform.position, player.transform.position) > minDist) {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+        } else {
+            animator.SetTrigger("Attack");
+            //animator.SetTrigger("Die");
+        }
+    }
+}
